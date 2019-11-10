14438fb7c1323ef90097aac1c22a9ee8c80536e9
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 21:46:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.collect{|words| words.capitlize + "!"}
+  array.collect{|words| words.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here

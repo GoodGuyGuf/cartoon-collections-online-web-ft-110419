09efe9cd4e774babcb15b3f0365d3d6a160f6a34
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 21:45:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ dwarves.each_with_index{|name, index| puts "#{index+1} #{name}"}
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect{|words| words.capitlize + "!"}
 end
 
 def long_planeteer_calls# code an argument here

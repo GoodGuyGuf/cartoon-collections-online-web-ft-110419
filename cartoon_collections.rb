aacc85f612ed13ea40b90cb3d6517f67f3cce679
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 21:48:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,13 @@ def summon_captain_planet(array)# code an argument here
   array.collect{|words| words.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any?{|any| any > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end

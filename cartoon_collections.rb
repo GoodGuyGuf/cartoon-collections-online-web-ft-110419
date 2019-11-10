6a27ee2a45573a4bf9a89a8c552b9dbf7f2bfd79
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 21:50:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any?{|any| any > 3}
+  array.any?{|any| any.index > 3}
 end
 
 def find_the_cheese(array)# code an argument here

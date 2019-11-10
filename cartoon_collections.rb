706247ba44797d1a112bcef75791c8f237f325e0
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 21:58:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  
+
 array.detect{|food| food.0?}
 end
